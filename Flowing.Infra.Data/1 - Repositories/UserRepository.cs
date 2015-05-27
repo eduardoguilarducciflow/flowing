94c4a00c4def7526f805119940074489d1c66c9d
eduardoguilarducci <eduardo.guiarducci@flowing.com.br>
2015-05-26 22:40:32 -0300

    add everything

---
+++ Flowing.Infra.Data/1 - Repositories/UserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Flowing.Domain.Entities;
+using Flowing.Domain.Interfaces.Repositories;
+
+namespace Flowing.Infra.Data.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+    }
+}
